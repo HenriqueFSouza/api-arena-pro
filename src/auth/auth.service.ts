3e8c4b7f2c56d7ba281c344f065a86de48862210
Henrique <henriquesouza432@outlook.com>
2025-04-24 08:57:54 -0300

    fix: temporaly removed same domain configuration for cookies

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcryptjs';
-import { ProfilesService } from 'src/profiles/profiles.service';
 import { Response } from 'express';
+import { ProfilesService } from 'src/profiles/profiles.service';
 
 interface AuthenticateRequest {
   email: string;
@@ -14,7 +14,7 @@ export class AuthService {
   constructor(
     private profilesService: ProfilesService,
     private jwtService: JwtService,
-  ) {}
+  ) { }
 
   async authenticate({ email, password }: AuthenticateRequest, response: Response) {
     const profile = await this.profilesService.findByEmail(email);
@@ -31,7 +31,7 @@ export class AuthService {
     response.cookie('arena_pro_access_token', accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: 'none',
       path: '/',
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
     });
