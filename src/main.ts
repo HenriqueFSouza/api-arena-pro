2bd6fcfbf330f51abc48709cce9e216a5a3f8dc5
Henrique <henriquesouza432@outlook.com>
2025-05-07 21:58:26 -0300

    feat: enable shutdown hooks in the NestJS application for graceful termination

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableShutdownHooks();
+
   const configService = app.get(ConfigService)
   const corsConfig = configService.get('cors')
   app.enableCors(corsConfig);
